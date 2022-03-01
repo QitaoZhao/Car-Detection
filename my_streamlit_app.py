50b794eba208c99f3b80b3b67b272dd4bd3f6627
QitaoZhao <790542646@qq.com>
2022-03-01 10:13:47 +0800

    Delete unnecessary codes.

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,14 +3,11 @@
 # More info: https://github.com/streamlit/demo-self-driving
 
 import cv2
-import os
 import psutil
 import pyowm
-import time
 import urllib
 from datetime import datetime, timedelta
 
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -51,45 +48,6 @@ def main():
 	elif app_mode == "Run the app":
 		readme_text.empty()
 		run_the_app()
-
-
-# This file downloader demonstrates Streamlit animation.
-def download_file(file_path):
-	# Don't download the file twice. (If possible, verify the download using the file length.)
-	if os.path.exists(file_path):
-		if "size" not in EXTERNAL_DEPENDENCIES[file_path]:
-			return
-		elif os.path.getsize(file_path) == EXTERNAL_DEPENDENCIES[file_path]["size"]:
-			return
-
-	# These are handles to two visual elements to animate.
-	weights_warning, progress_bar = None, None
-	try:
-		weights_warning = st.warning("Downloading %s..." % file_path)
-		progress_bar = st.progress(0)
-		with open(file_path, "wb") as output_file:
-			with urllib.request.urlopen(EXTERNAL_DEPENDENCIES[file_path]["url"]) as response:
-				length = int(response.info()["Content-Length"])
-				counter = 0.0
-				MEGABYTES = 2.0 ** 20.0
-				while True:
-					data = response.read(8192)
-					if not data:
-						break
-					counter += len(data)
-					output_file.write(data)
-
-					# We perform animation by overwriting the elements.
-					weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
-											(file_path, counter / MEGABYTES, length / MEGABYTES))
-					progress_bar.progress(min(counter / length, 1.0))
-
-	# Finally, we remove these visual elements by calling .empty().
-	finally:
-		if weights_warning is not None:
-			weights_warning.empty()
-		if progress_bar is not None:
-			progress_bar.empty()
 
 
 # This is the main app app itself, which appears when the user selects "Run the app".
@@ -315,10 +273,6 @@ def run_the_app():
 	st.line_chart(chart_data, width=260, height=250)
 	st.write("😆😆 Enjoy yourself!!!")
 	c = st.empty()
-
-	# for i in range(1, 30):
-	#     time.sleep(0.5) #just here so you can see the change
-	#     c.text(i)
 
 	# Avoid re-running everything
 	st.session_state.first_time = False
